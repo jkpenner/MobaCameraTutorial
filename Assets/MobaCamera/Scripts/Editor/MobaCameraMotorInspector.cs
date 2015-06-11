fcf692320bc87dd323e7671fff69de9295090dcd
Jacob Penner <jkpenner91@gmail.com>
2015-06-10 22:33:12 -0700

    Initial Unity Project

---
+++ Assets/MobaCamera/Scripts/Editor/MobaCameraMotorInspector.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(MobaCameraMotor))]
+public class MobaCameraMotorInspector : Editor {
+    static public bool RequiredFoldout {
+        get { return EditorPrefs.GetBool("MobaCameraInspector_RequiredFoldout", true); }
+        set { EditorPrefs.SetBool("MobaCameraInspector_RequiredFoldout", value); }
+    }
+
+    public override void OnInspectorGUI() {
+        EditorGUILayout.Space();
+
+        RequiredFoldout = GUILayout.Toggle(RequiredFoldout, "Required", EditorStyles.toolbarButton);
+        if (RequiredFoldout) {
+            DisplayRequiredGUI((MobaCameraMotor)target);
+        }
+
+        EditorGUILayout.Space();
+    }
+
+    static public void DisplayRequiredGUI(MobaCameraMotor target) {
+        EditorGUI.BeginChangeCheck();
+
+        var newPivot = EditorGUILayout.ObjectField("Pivot", target.Pivot, typeof(Transform), true) as Transform;
+        var newOffset = EditorGUILayout.ObjectField("Offset", target.Offset, typeof(Transform), true) as Transform;
+        var newCamera = EditorGUILayout.ObjectField("Camera", target.TargetCamera, typeof(Camera), true) as Camera;
+
+        if (EditorGUI.EndChangeCheck()) {
+            Undo.RecordObject(target, "Modify MobaCamera Requirements");
+            target.Pivot = newPivot;
+            target.Offset = newOffset;
+            target.TargetCamera = newCamera;
+            EditorUtility.SetDirty(target);
+        }
+
+        if (GUILayout.Button("Auto Generate", EditorStyles.miniButton)) {
+            GenerateAuto(target);
+        }
+
+        if (GUILayout.Button("Generate from Main Camera", EditorStyles.miniButton)) {
+            GenerateFromMainCamera(target);
+        }
+    }
+
+    static public void GenerateAuto(MobaCameraMotor target) {
+        if (target.Pivot == null) {
+            target.Pivot = target.gameObject.transform;
+        }
+
+        if (target.Offset == null) {
+            target.Offset = new GameObject("Offset").transform;
+        }
+
+        if (target.TargetCamera == null) {
+            GameObject tempGO = new GameObject("MobaCamera");
+            target.TargetCamera = tempGO.AddComponent<Camera>();
+        }
+
+        target.TargetCamera.transform.parent = target.Offset;
+        target.Offset.parent = target.Pivot;
+
+        target.TargetCamera.transform.localPosition = Vector3.zero;
+        target.TargetCamera.transform.localRotation = Quaternion.identity;
+
+        target.Offset.localPosition = Vector3.zero;
+        target.Offset.localRotation = Quaternion.identity;
+    }
+
+    static public void GenerateFromMainCamera(MobaCameraMotor target) {
+        target.TargetCamera = Camera.main;
+        GenerateAuto(target);
+    }
+}
---
+++ Assets/MobaCamera/Scripts/MobaCameraMotor.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobaCameraMotor : MonoBehaviour {
+    private Transform _pivot = null;
+    private Transform _offset = null;
+    private Camera _camera = null;
+
+    public Transform Pivot {
+        get { return _pivot; }
+        set { _pivot = value; }
+    }
+
+    public Transform Offset {
+        get { return _offset; }
+        set { _offset = value; }
+    }
+
+    public Camera TargetCamera {
+        get { return _camera; }
+        set { _camera = value; }
+    }
+
+    private void Awake() {
+        if (_pivot == null || _offset == null || _camera == null) {
+            string missing = "";
+            if (_pivot == null) {
+                missing += "Pivot";
+                this.enabled = false;
+            }
+
+            if (_offset == null) {
+                missing += (missing == "" ? "" : ", ") + "Offset";
+                this.enabled = false;
+            }
+
+            if (_camera == null) {
+                missing += (missing == "" ? "" : ", ") + "Camera";
+                this.enabled = false;
+            }
+
+            Debug.LogWarning("Moba_Camera Requirements Missing: " + missing +
+                ". Add missing objects to the requirement tab under the Moba_camera script in the Inspector.");
+        } else {
+            if (Offset.parent != Pivot) {
+                Offset.parent = Pivot;
+            }
+
+            if (TargetCamera.transform.parent != Offset) {
+                TargetCamera.transform.parent = Offset;
+            }
+        }
+    }
+}
